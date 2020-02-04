3d88508c0e80450dbad83781380b22a13957e903
Thomas Nourse <tgnourse@gmail.com>
2020-02-03 20:44:49 -0800

    A couple updates.

--- read-power.py
+++ read-power.py
@@ -36,7 +36,7 @@ def parse_field(data):
         if b in m:
             f = f + m[b]
         else:
-            f = f + ''
+            f = f + '#'
     return f
 
 
@@ -67,11 +67,11 @@ def parse_meter(data):
         'power_phase_1': parse_decimal_field(data[126:132]),
         'power_phase_2': parse_decimal_field(data[132:138]),
         'power_phase_3': parse_decimal_field(data[138:144]),
-        'power_factor_phase_1': parse_decimal_field(data[144:148]),
-        'power_factor_phase_2': parse_decimal_field(data[148:152]),
-        'power_factor_phase_3': parse_decimal_field(data[152:156]),
-        'demand_max': parse_decimal_field(data[156:162]),
-        'demand_period': parse_decimal_field(data[162:163]),
+        'power_factor_phase_1': parse_field(data[144:148]),
+        'power_factor_phase_2': parse_field(data[148:152]),
+        'power_factor_phase_3': parse_field(data[152:156]),
+        'demand_max': parse_field(data[156:162]),
+        'demand_period': parse_field(data[162:163]),
         'date_year': parse_field(data[163:165]),
         'date_month': parse_field(data[165:167]),
         'date_day': parse_field(data[167:169]),
@@ -82,33 +82,38 @@ def parse_meter(data):
     }
 
 
+def read_meters(port, ids):
+    ser = serial.Serial(port)
+    print(ser.name)
+
+    for meter_id in ids:
+        print(f'reading {id}')
+        ser.write(b'/?' + meter_id.encode('ascii') + b'!\r\n')
+        pprint.pprint(parse_meter(ser.read(255)))
+
+    ser.close()
+
+
 def test_parsing():
-    # reading 000400003705
+    # 000400003705
     meter1 = b'\x82\x90"\x17000\xb400003\xb705000\xb13\xb1\xb1\xb70000\xb80600000505\xb700000000000000000000000000000000000000000000000000000000\xb1\xb2\xb20\xb1\xb2\xb1\xb7000000000000360000000000\xb1\xb4000036\xb2000000000003\xb7\xb8\xc3099\xcc099\xc3000000\xb23\xb400\xb1\xb200\xb2030\xb2\xb1\xb209\xb2\xb10\xb2000000000000000000000000000000000000000000000000000000000000\x00!\x8d\n\x03`\xc0'
     print(meter1)
     print(meter1.hex())
-    # reading 000400003718
+
+    it = iter(meter1.hex())
+    for b1 in it:
+        print(next(it), end="")
+    print('')
+    # 000400003718
     meter2 = b'\x82\x90"\x17000\xb400003\xb7\xb1\xb8000035050000\xb2\xb1\xb8\xb80000\xb13\xb1\xb700000000000000000000000000000000000000000000000000000000\xb1\xb2\xb1900000000000\xb1000000000000000\xb1\xb10000000000000000000\xb1\xb10\xc309\xb4\xc3000\xc30000000\xb2600\xb1\xb200\xb20\xb20\xb1\xb20\xb1\xb1\xb2\xb20\xb2000000000000000000000000000000000000000000000000000000000000\x00!\x8d\n\x03\x84\x81'
 
     pprint.pprint(parse_meter(meter1))
     pprint.pprint(parse_meter(meter2))
 
 
-def read_meter():
-    #ser = serial.Serial('/dev/tty.usbserial-141320')  # open serial port on Mac
-    ser = serial.Serial('/dev/ttyUSB1')  # open serial port on RPi
-    print(ser.name)
-
-    print("reading 000400003705")
-    ser.write(b'/?000400003705!\r\n')
-    pprint.pprint(parse_meter(ser.read(255)))
-
-    print("reading 000400003718")
-    ser.write(b'/?000400003718!\r\n')
-    pprint.pprint(parse_meter(ser.read(255)))
-
-    ser.close()
-
+def test_read_meters():
+    # on mac: /dev/tty.usbserial-141320
+    read_meters('/dev/ttyUSB1', ['000400003705', '000400003718'])
 
 # test_parsing()
 read_meter()
